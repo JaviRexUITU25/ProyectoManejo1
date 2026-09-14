0d1b9685a191f4c04af48d6aa5ecfff4e79ba326
André <javiandre2007@gmail.com>
2026-09-14 14:03:50 -0600

    Interfaz del usuario

--- settingWin.py
+++ settingWin.py
@@ -1,1 +1,9 @@
-print("Ventana de setting con su interfaz (customtinker)")
+import os
+import customtkinter as ctk
+from tkinter import colorchooser, filedialog, messagebox
+
+from i18n import t
+
+class SettingsWindow(ctk.CTkToplevel):
+    def __init__(self, master, config: dict, on_guardar):
+        
