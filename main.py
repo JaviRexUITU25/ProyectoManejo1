deaf2e7e400beca79c9c93efa033232ffd1c00c8
André <javiandre2007@gmail.com>
2026-09-16 15:37:17 -0600

    Correccion de errores en los nombres de variables, referencias y llamados a las funciones

--- main.py
+++ main.py
@@ -37,102 +37,103 @@ class Aplicacion(ctk.CTk):
         self._aplicar_configuracion()
 
         # Si pasa algun error o no hay archivo
-        if resultado_carga.status not in ("not_found"):
+        if resultado_carga.status not in ("not_found", "ok"):
             self.after(300, lambda: messagebox.showwarning(
                 t(self.idioma, "aviso_titulo"), resultado_carga.message))
 
-        # MENU PRINCIPAL ------------------------------------------------------------
-        def _construir_barra_menu(self):
-            self.frame_memu = ctk.CTkFrame(self, height =44, corner_radius = 0)
-            self.frame_menu.pack(side="top", fill="x")
-            self.frame_menu.pack_propagate(False)
+    # MENU PRINCIPAL ------------------------------------------------------------
+    def _construir_barra_menu(self):
+        self.frame_menu = ctk.CTkFrame(self, height =44, corner_radius = 0)
+        self.frame_menu.pack(side="top", fill="x")
+        self.frame_menu.pack_propagate(False)
 
-            self.botones_menu = {}
-            especificaciones = [
-                ("menu_archivo", self._abrir_menu_archivo),
-                ("menu_edicion", self._abrir_menu_edicion),
-                ("menu_ver", self._abrir_menu_ver),
-                ("menu_settings", self._abrir_settings),
-            ]
-            for i, (clave, comando) in enumerate(especificaciones):
-                btn = ctk.CTkButton(self.frame_menu, text=t(self.idioma,clave),
-                                    width = 95, corner_radius=6,
-                                    fg_color = "transparent", command=comando)
-                btn.pack(side="left", padx=(14 if i == 0 else 4,4),pady=7)
-                self.botones_menu[clave] = btn
+        self.botones_menu = {}
+        especificaciones = [
+            ("menu_archivo", self._abrir_menu_archivo),
+            ("menu_edicion", self._abrir_menu_edicion),
+            ("menu_ver", self._abrir_menu_ver),
+            ("menu_settings", self._abrir_settings),
+        ]
+        for i, (clave, comando) in enumerate(especificaciones):
+            btn = ctk.CTkButton(self.frame_menu, text=t(self.idioma,clave),
+                                width = 95, corner_radius=6,
+                                fg_color = "transparent", command=comando)
+            btn.pack(side="left", padx=(14 if i == 0 else 4,4),pady=7)
+            self.botones_menu[clave] = btn
 
-        def _popup_simulado(self, opciones):
-            menu = Menu(self, tearoff=0)
-            for etiqueta, comando_real in opciones:
-                menu.add_command(label=etiqueta, command=comando_real or self._mostrar_aviso_simulado)
-                return menu
+    def _popup_simulado(self, opciones):
+        menu = Menu(self, tearoff=0)
+        for etiqueta, comando_real in opciones:
+            menu.add_command(label=etiqueta, command=comando_real or self._mostrar_aviso)
+        return menu
 
-        def _abrir_menu_archivo(self):
-            opciones = [
-                (t(self.idioma, "archivo_nuevo"), None),
-                (t(self.idioma, "archivo_abrir"), None),
-                (t(self.idioma, "archivo_guardar"), None),
-                (t(self.idioma, "archivo_guardar_como"), None),
-                (t(self.idioma, "archivo_salir"), self._salir),
-            ]
-            self._mostrar_popup(self.botones_menu["menu_archivo"], opciones)
+    def _abrir_menu_archivo(self):
+        opciones = [
+            (t(self.idioma, "archivo_nuevo"), None),
+            (t(self.idioma, "archivo_abrir"), None),
+            (t(self.idioma, "archivo_guardar"), None),
+            (t(self.idioma, "archivo_guardar_como"), None),
+            (t(self.idioma, "archivo_salir"), self._salir),
+        ]
+        self._mostrar_popup(self.botones_menu["menu_archivo"], opciones)
 
-        def _abrir_menu_edicion(self):
-            opciones = [
-                (t(self.idioma, "edicion_deshacer"), None),
-                (t(self.idioma, "edicion_rehacer"), None),
-                (t(self.idioma, "edicion_cortar"), None),
-                (t(self.idioma, "edicion_copiar"), None),
-                (t(self.idioma, "edicion_pegar"), None),
-            ]
-            self._mostrar_popup(self.botones_menu["menu_edicion"], opciones)
+    def _abrir_menu_edicion(self):
+        opciones = [
+            (t(self.idioma, "edicion_deshacer"), None),
+            (t(self.idioma, "edicion_rehacer"), None),
+            (t(self.idioma, "edicion_cortar"), None),
+            (t(self.idioma, "edicion_copiar"), None),
+            (t(self.idioma, "edicion_pegar"), None),
+        ]
+        self._mostrar_popup(self.botones_menu["menu_edicion"], opciones)
 
-        def _abrir_menu_ver(self):
-            opciones = [
-            (t(self.idioma, "ver_pantalla_completa"), None),
-            (t(self.idioma, "ver_zoom_mas"), None),
-            (t(self.idioma, "ver_zoom_menos"), None),
-            (t(self.idioma, "ver_barra_estado"), None),
-            ]
-            self._mostrar_popup(self.botones_menu["menu_ver"], opciones)
+    def _abrir_menu_ver(self):
+        opciones = [
+        (t(self.idioma, "ver_pantalla_completa"), None),
+        (t(self.idioma, "ver_zoom_mas"), None),
+        (t(self.idioma, "ver_zoom_menos"), None),
+        (t(self.idioma, "ver_barra_estado"), None),
+        ]
+        self._mostrar_popup(self.botones_menu["menu_ver"], opciones)
 
-        def _mostrar_popup(self,boton,opciones):
-            menu = self._popup_simulado(opciones)
-            x = boton.winfo_rootx()
-            y = boton.winfo_rooty() + boton.winfo_height()
-            try:
-                menu.tk_popup(x.y)
-            finally:
-                menu.grab_release()
-        def _mostrar_aviso(self):
-            messagebox.showinfo(t(self.idioma, "titulo_simulado"), t(self.idioma, "msg_simulado"))
+    def _mostrar_popup(self, boton, opciones):
+        menu = self._popup_simulado(opciones)
+        x = boton.winfo_rootx()
+        y = boton.winfo_rooty() + boton.winfo_height()
+        try:
+            menu.tk_popup(x, y)
+        finally:
+            menu.grab_release()
+            
+    def _mostrar_aviso(self):
+        messagebox.showinfo(t(self.idioma, "simulado_titulo"), t(self.idioma, "simulado_msg"))
 
-        def _salir(self):
-            self.destroy()
+    def _salir(self):
+        self.destroy()
 
-        # SETTINGS (ESTO DEBE SER FUNCIONAL)
-        def _abrir_settings(self):
-            self._settings_win = SettingsWin(self, self.config, on_guardar=self._guardar)
+    # SETTINGS (ESTO DEBE SER FUNCIONAL)
+    def _abrir_settings(self):
+        self._settings_win = SettingsWindow(self, self.config, on_guardar=self._guardar)
 
-        def _guardar(self, nuevo_config, ventana):
-            resultado = config_manager.save_config(nuevo_config)
+    def _guardar(self, nuevo_config, ventana):
+        resultado = Config.save_config(nuevo_config)
 
-            if resultado.ok:
-                self.config = resultado.data
-                self.idioma = self.config.get("idioma", "es-ES")
-                self._aplicar_configuraciion()
-                self._reconstruir_textos_menu()
-                messagebox.showinfo(t(self.idioma, "aviso_titulo"),
-                                    t(self.idioma, "settings_guardado"))
-                ventana.destroy()
-            else:
-                # Por si no guarda ijjijia
-                messagebox.showerror(t(self.idioma, "aviso_titulo"),
-                                     t(self.idioma, "settings_guardado_error",
-                                       detalle=resultado.message))
+        if resultado.ok:
+            self.config = resultado.data
+            self.idioma = self.config.get("idioma", "es-ES")
+            self._aplicar_configuracion()
+            self._reconstruir_textos_menu()
+            messagebox.showinfo(t(self.idioma, "aviso_titulo"),
+                                t(self.idioma, "settings_guardado_ok"))
+            ventana.destroy()
+        else:
+            # Por si no guarda
+            messagebox.showerror(t(self.idioma, "aviso_titulo"),
+                                 t(self.idioma, "settings_guardado_error",
+                                   detalle=resultado.message))
 
     # Contenido principal
-    def _consrtuir_contenido(self):
+    def _construir_contenido(self):
         self.frame_contenido = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.frame_contenido.pack(fill="both", expand=True, padx=30, pady=26)
 
@@ -158,14 +159,14 @@ class Aplicacion(ctk.CTk):
             self.filas_resumen[clave] = fila
         ctk.CTkLabel(self.tarjeta, text= "").pack(pady=8)
 
-    def _recontruir_textos_menu(self):
+    def _reconstruir_textos_menu(self):
         for clave, btn in self.botones_menu.items():
             btn.configure(text=t(self.idioma, clave))
-        self.tittle(t(self.idioma, "titulo_app"))
+        self.title(t(self.idioma, "app_title"))
 
     def _actualizar_resumen(self):
         nombre = self.config.get("nombre_usuario", "")
-        self.lbl_bienvenida.configure(text=t(self.idioma, "binevenida", nombre=nombre))
+        self.lbl_bienvenida.configure(text=t(self.idioma, "bienvenida", nombre=nombre))
         self.lbl_resumen_titulo.configure(text=t(self.idioma, "resumen_titulo"))
 
         tema = self.config.get("tema_interfaz", "claro")
@@ -191,7 +192,7 @@ class Aplicacion(ctk.CTk):
             try:
                 img = Image.open(ruta).convert("RGB")
                 img.thumbnail((112,112))
-                self._imagen_perfil_ctk = ctk.CTkImage(light_image=img, dar_image=img, size=img.size)
+                self._imagen_perfil_ctk = ctk.CTkImage(light_image=img, dark_image=img, size=img.size)
                 self.lbl_avatar.configure(image=self._imagen_perfil_ctk,text="")
                 return
             except Exception:
@@ -199,13 +200,10 @@ class Aplicacion(ctk.CTk):
         self._imagen_perfil_ctk = None
         self.lbl_avatar.configure(image=None, text="👤", font=ctk.CTkFont(size=52))
 
-
-
-#Aplicar la configuracion a la interfaz
-
+    #Aplicar la configuracion a la interfaz
     def _aplicar_configuracion(self):
         modo = "Dark" if self.config.get("tema_interfaz") == "oscuro" else "Light"
-        ctk.set_appearence_mode(modo)
+        ctk.set_appearance_mode(modo)
 
         color_menu = self.config.get("color_menu", "#2C3E50")
         color_letra = self.config.get("color_letra", "#FFFFFF")
